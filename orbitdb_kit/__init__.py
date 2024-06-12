ab551563a7010cf703dcca2c92e80f19b4089ab3
endomorphosis <starworks5@gmail.com>
2024-06-12 00:15:58 -0700

    Refactor import statement for config in __init__.py

--- orbitdb_kit/__init__.py
+++ orbitdb_kit/__init__.py
@@ -1,5 +1,5 @@
 from .orbitdb_kit import orbitdb_kit
-from .config import config
+from config.config import config
 from .orbitdb_kit_lib import orbitdb_kit_lib
 # from .websocket_kit import websocket_kit
 # from .websocket_kit_lib import websocket_kit_lib
