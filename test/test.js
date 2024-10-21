e04f4dd653cded5069db03ff215f6561f74413a4
endomorphosis <starworks5@gmail.com>
2024-10-20 17:09:45 -0700

    chore: Bump package version to 1.0.18 and update libp2p_kit_js class

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 import { orbitDbKitJs } from "../orbitdb_kit_js/orbitdb_kit.js"
-import { libp2pKit } from "libp2p_kit_js";
+import { libp2pKitJs } from "libp2p_kit_js";
 import { ipfsKitJs } from 'ipfs_kit_js'
 // import { ipfsKitCjs } from 'ipfs_kit_cjs'
 // import { requireConfig } from "../config/config.js";
@@ -13,7 +13,7 @@ export class test_orbitdb_kit_js {
     constructor(resources, metadata) {
         this.imports = {};
         // this.config = requireConfig();
-        this.libp2pKit = new libp2pKit(resources, metadata);
+        this.libp2pKitJs = new libp2pKitJs(resources, metadata);
         // this.ipfsKitCjs = new ipfsKitCjs(resources, metadata);
         this.ipfsKitJs = new ipfsKitJs(resources, metadata);
         this.orbitDbKitJs = new orbitDbKitJs(resources, metadata);
