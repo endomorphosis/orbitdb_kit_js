ad3c55453daed399bd72838b21443340547dae4d
endomorphosis <starworks5@gmail.com>
2024-10-26 00:23:08 -0700

    refactor: Update dependencies in package.json and package-lock.json

--- orbitdb_kit_js/main.js
+++ orbitdb_kit_js/main.js
@@ -10,8 +10,6 @@ import pvtsutils from "pvtsutils";
 import pvutils from "pvutils";
 import { requireConfig } from '../config/config.js';
 import { randomInt } from 'crypto';
-
-
 import {createLibp2p} from 'libp2p'
 import {identify} from '@libp2p/identify'
 import {gossipsub} from '@chainsafe/libp2p-gossipsub'
@@ -20,6 +18,7 @@ import {mdns} from '@libp2p/mdns'
 import { webSockets } from '@libp2p/websockets';
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
+import { mplex } from '@libp2p/mplex'
 import { bootstrap } from '@libp2p/bootstrap'
 import { floodsub } from '@libp2p/floodsub'
 import { kadDHT, removePublicAddressesMapper } from '@libp2p/kad-dht'
@@ -52,7 +51,8 @@ export class orbitDbKitJs {
 			peerHandlers: [],
 			messageHistory: [],
 			libp2p: null,
-            dbname: null
+            dbname: null,
+            bootstrappers: []
 		}
     }
 
@@ -63,10 +63,10 @@ export class orbitDbKitJs {
                 this.config = requireConfig();
             }
             if (this.config.identityKey){
-                this.ctx.config.identityKey = this.config.identityKey;
+                this.ctx.config.identityKey = this.config.libp2p.identityKey;
             }
             else{
-                this.ctx.config.identityKey = null;
+                this.ctx.config.identityKey = createFromPrivKey(deriveKeyPair(id))
             }
             if (this.config.listen){
                 this.ctx.config.listen = this.config.listen
@@ -98,30 +98,17 @@ export class orbitDbKitJs {
             else{
                 this.ctx.dbname = "test"
             }
-            await this.libp2pKit.init(this.ctx);
-            // this.ctx.libp2p = await createLibp2p({
-            //     peerId: this.ctx.config.identityKey
-            //         ? await createFromPrivKey(deriveKeyPair(this.ctx.config.identityKey))
-            //         : undefined,
-            //     addresses: {
-            //         listen: this.ctx.config.listen
-            //     },
-            //     transports: [
-            //         tcp(),
-            //         webSockets()
-            //     ],
-            //     streamMuxers: [
-            //         mplex()
-            //     ],
-            //     connectionEncryption: [
-            //         noise()
-            //     ]
-            // })
+            if (this.config.libp2p.bootstrappers){
+                this.ctx.bootstrappers = this.config.libp2p.bootstrappers
+            }
+            else{
+                this.ctx.bootstrappers = []
+            }
         }
         else{
             this.ctx = ctx;
             if (!this.ctx.config.identityKey){
-                this.ctx.config.identityKey = null;
+                this.ctx.config.identityKey =  createFromPrivKey(deriveKeyPair(id))
             }
             if (!this.ctx.config.listen){
                 this.ctx.config.listen = "/ip4/0.0.0.0/tcp/0"
@@ -137,42 +124,20 @@ export class orbitDbKitJs {
             }
             if (!this.ctx.dbname){
                 this.ctx.dbname = "test"
-            }            
-            await this.libp2pKit.init(this.ctx);
+            }
+            if (!this.ctx.bootstrappers){
+                this.ctx.bootstrappers = this.config.bootstrappers
+            }
         }
 
+        await this.libp2pKit.init(this.ctx);
 
-        // let blockstore = new LevelBlockstore(`./ipfs/`+id+`/blocks`);
-        // this.blockstore = blockstore;
-        // let datastore = new LevelDatastore(`./ipfs/`+id+`/datastore`);
-        // this.datastore = datastore;
-        // this.ctx.libp2p.addEventListener("peer:connect", event => {
-        //     console.log('connected', event.detail)
-        // })
-        // let ipfs = await createHelia({blockstore: blockstore, libp2p: this.ctx.libp2p, datastore: datastore, blockBrokers: [bitswap()]})
-        // this.ipfs = ipfs
-        // ipfs.libp2p.addEventListener("peer:connect", event => {
-        //     console.log('connected', event.detail)
-        // })
-        // let identities = await Identities({ ipfs, path: `./orbitdb/`+id+`/identities` })
-        // this.identities = identities
-        // let identity = await identities.createIdentity({ id })
-        // this.identity = identity
-        // let orbitdb = await createOrbitDB({ipfs: ipfs, identities, id: id, directory: `./orbitdb/`+id})
-        // this.orbitdb = orbitdb 
-
-        // this.orbitdb = await createOrbitDB(ipfs, { identity: identity, accessController: OrbitDBAccessController })
-        // let db = await orbitdb.open(this.ctx.dbname,
-        //     {type: 'documents',
-        //         AccessController: OrbitDBAccessController({ write: ["*"], sync: false}),
-        //     })
-        // this.db = db
-        // let oldHeads = await db.log.heads()
-        // this.oldHeads = oldHeads
-
-        const ipfsLibp2pOptions = {
+        let libp2p = await createLibp2p({
+            peerId: this.ctx.config.identityKey
+                    ? await createFromPrivKey(deriveKeyPair(this.ctx.config.identityKey))
+                    : undefined,
             addresses: {
-                listen: ['/ip4/0.0.0.0/tcp/0']
+                listen: this.ctx.config.listen
             },
             transports: [
                 tcp(),
@@ -199,7 +164,7 @@ export class orbitDbKitJs {
                     interval: 1000
                 }),
                 bootstrap({
-                    list: bootstrappers
+                    list: this.ctx.bootstrappers
                 })
             ],
             services: {
@@ -220,28 +185,133 @@ export class orbitDbKitJs {
             connectionManager: {
 
             }
-        }
-        const libp2p = await createLibp2p({  addresses: {
-            //listen: [`/ip4/${ipAddress}/tcp/0`]
-            listen: ['/ip4/0.0.0.0/tcp/0']
-            }, ...ipfsLibp2pOptions})
-        const blockstore = new LevelBlockstore(`./ipfs/`+id+`/blocks`)
-        const datastore = new LevelDatastore(`./ipfs/`+id+`/datastore`);
-        const ipfs = await createHelia({blockstore: blockstore, libp2p: libp2p, datastore: datastore, blockBrokers: [bitswap()]})
-        const identities = await Identities({ ipfs, path: `./orbitdb/`+id+`/identities` })
-        const identity = identities.createIdentity({ id })
-        ipfs.libp2p.addEventListener("peer:connect", event => {
+        })
+        
+        // let libp2p = await createLibp2p({
+        //     peerId: this.ctx.config.identityKey
+        //         ? await createFromPrivKey(deriveKeyPair(this.ctx.config.identityKey))
+        //         : undefined,
+        //     addresses: {
+        //         listen: this.ctx.config.listen
+        //     },
+        //     transports: [
+        //         tcp(),
+        //         webSockets()
+        //     ],
+        //     streamMuxers: [
+        //         mplex()
+        //     ],
+        //     connectionEncryption: [
+        //         noise()
+        //     ]
+        // })
+
+        this.ctx.libp2p = libp2p
+
+
+        let blockstore = new LevelBlockstore(`./ipfs/`+id+`/blocks`);
+        this.blockstore = blockstore;
+        let datastore = new LevelDatastore(`./ipfs/`+id+`/datastore`);
+        this.datastore = datastore;
+        this.ctx.libp2p.addEventListener("peer:connect", event => {
             console.log('connected', event.detail)
         })
+        let ipfs = await createHelia({blockstore: blockstore, libp2p: libp2p, datastore: datastore, blockBrokers: [bitswap()]})
+        this.ipfs = ipfs
+        // ipfs.libp2p.addEventListener("peer:connect", event => {
+        //     console.log('connected', event.detail)
+        // })
+        let identities = await Identities({ ipfs, path: `./orbitdb/`+id+`/identities` })
+        this.identities = identities
+        let identity = await identities.createIdentity({ id })
+        this.identity = identity
 
-        const orbitdb = await createOrbitDB({ipfs: ipfs, identities, id: id, directory: `./orbitdb/`+id})
+        // let orbitdb = await createOrbitDB({ipfs: ipfs, identities, id: id, directory: `./orbitdb/`+id})
+        // this.orbitdb = orbitdb 
 
-        const db = await orbitdb.open(this.ctx.dbname,
+        let orbitdb = await createOrbitDB({ ipfs: ipfs, identity: identity, accessController: OrbitDBAccessController })
+        this.orbitdb = orbitdb
+
+        let db = await orbitdb.open(this.ctx.dbname,
             {type: 'documents',
                 AccessController: OrbitDBAccessController({ write: ["*"], sync: false}),
             })
+        this.db = db
         let oldHeads = await db.log.heads()
-    
+        this.oldHeads = oldHeads
+
+        // const ipfsLibp2pOptions = {
+        //     addresses: {
+        //         listen: ['/ip4/0.0.0.0/tcp/0']
+        //     },
+        //     transports: [
+        //         tcp(),
+        //         webSockets({
+        //             // filter: all
+        //         }),
+        //         // webRTC(),
+        //         circuitRelayTransport({
+        //             discoverRelays: 1
+        //         })
+        //     ],
+        //     streamMuxers: [
+        //         yamux(),
+        //         mplex()
+        //     ],
+        //     connectionEncryption: [
+        //         noise()
+        //     ],
+        //     peerDiscovery: [
+        //         mdns({
+        //             interval: 20e3
+        //         }),
+        //         pubsubPeerDiscovery({
+        //             interval: 1000
+        //         }),
+        //         bootstrap({
+        //             list: this.ctx.bootstrappers
+        //         })
+        //     ],
+        //     services: {
+        //         lanDHT: kadDHT({
+        //             protocol: '/ipfs/lan/kad/1.0.0',
+        //             peerInfoMapper: removePublicAddressesMapper,
+        //             clientMode: false
+        //         }),
+        //         pubsub:
+        //             gossipsub({
+        //                 allowPublishToZeroPeers: true
+        //             }),
+        //         identify: identify(),
+        //         ping: ping({
+        //             protocolPrefix: 'ipfs', // default
+        //         }),
+        //     },
+        //     connectionManager: {
+
+        //     }
+        // }
+        // const libp2p = await createLibp2p({  addresses: {
+        //     //listen: [`/ip4/${ipAddress}/tcp/0`]
+        //     listen: ['/ip4/0.0.0.0/tcp/0']
+        //     }, ...ipfsLibp2pOptions})
+        // const blockstore = new LevelBlockstore(`./ipfs/`+id+`/blocks`)
+        // const datastore = new LevelDatastore(`./ipfs/`+id+`/datastore`);
+        // const ipfs = await createHelia({blockstore: blockstore, libp2p: libp2p, datastore: datastore, blockBrokers: [bitswap()]})
+        // const identities = await Identities({ ipfs, path: `./orbitdb/`+id+`/identities` })
+        // const identity = identities.createIdentity({ id })
+        // ipfs.libp2p.addEventListener("peer:connect", event => {
+        //     console.log('connected', event.detail)
+        // })
+
+        // const orbitdb = await createOrbitDB({ipfs: ipfs, identities, id: id, directory: `./orbitdb/`+id})
+
+        // const db = await orbitdb.open(this.ctx.dbname,
+        //     {type: 'documents',
+        //         AccessController: OrbitDBAccessController({ write: ["*"], sync: false}),
+        //     })
+        // let oldHeads = await db.log.heads()
+        return {orbitdb: orbitdb, db: db, oldHeads: oldHeads}
     }
 
     async close() {
